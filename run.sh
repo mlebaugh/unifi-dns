2d17791a4835181c9939f0fea1a5fab46d912cf3
mlebaugh <mattlebaugh@gmail.com>
2020-12-26 10:31:41 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,15 @@ hosts_dir=/etc/dnsmasq.hosts
 unifi_hosts=$hosts_dir/unifi.hosts
 
 [ -d $hosts_dir ] || mkdir $hosts_dir
-webproc --config /etc/dns/dnsmasq.conf -- dnsmasq --no-daemon & 
+rm /etc/dnsmasq.conf
+ln -s /etc/dns/dnsmasq.conf /etc/dnsmasq.conf
+webproc --config /etc/dnsmasq.conf -- dnsmasq --no-daemon & 
 
 while true; do
     ./get_unifi_reservations.py > /tmp/current_unifi.hosts
     if [ $? = 0 ] && ! diff -N $unifi_hosts /tmp/current_unifi.hosts; then
         mv /tmp/current_unifi.hosts $unifi_hosts
-        kill -9 $(pidof webproc) && kill -9 $(pidof dnsmasq) && webproc --config /etc/dns/dnsmasq.conf -- dnsmasq --no-daemon &
+        kill -9 $(pidof webproc) && kill -9 $(pidof dnsmasq) && webproc --config /etc/dnsmasq.conf -- dnsmasq --no-daemon &
     fi
     sleep ${UNIFI_POLL_INTERVAL:-60}
 done
