55801558b19a4394ad43190ac1a2db788a1b1712
mlebaugh <mattlebaugh@gmail.com>
2021-07-02 13:21:41 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ while true; do
     ./get_unifi_reservations.py > /tmp/current_unifi.hosts
     if [ $? = 0 ] && ! diff -N $unifi_hosts /tmp/current_unifi.hosts; then
         mv /tmp/current_unifi.hosts $unifi_hosts
-        kill -9 $(pidof webproc) && kill -9 $(pidof dnsmasq) && webproc --config /etc/dnsmasq.conf -- dnsmasq --no-daemon &
+        pkill -9 $(pidof webproc) && pkill -9 $(pidof dnsmasq) && webproc --config /etc/dnsmasq.conf -- dnsmasq --no-daemon &
     fi
     sleep ${UNIFI_POLL_INTERVAL:-60}
 done
